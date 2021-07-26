6acbeabec36b3cc39afbb1db0b56c095a32738a4
CotherArt <cother@protonmail.com>
2021-07-26 13:36:47 -0600

    first commit

---
+++ LCDScript/LCDScript.ino
@@ -0,0 +1,65 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int relay = 10, pot = 0;
+int seviche_pin = 10;
+int seviche = 0;
+
+int val = 0;
+String time;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// LCD dimentions
+const int xlen = 16, ylen = 2;
+
+int incomingByte = 0; // for incoming serial data
+String incomingString = "";
+
+void timer_message(){
+    
+}
+
+void centerMsg(String msg, byte pos){
+  int center = xlen / 2 - msg.length() / 2 - 1;
+  lcd.setCursor(center,pos);
+  lcd.print(msg);
+}
+
+void timeMode(){
+  centerMsg("---Tiempo---", 0);
+  time = map(analogRead(pot), 0, 1023, 0, 60);
+  centerMsg(time, 1);
+  delay(100);
+}
+
+void ini_msg(){
+  centerMsg("Welcome to", 0);
+  centerMsg("Coffee timer uwu", 1);
+}
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(xlen, ylen);
+  pinMode(relay, OUTPUT);
+  pinMode(seviche_pin, INPUT_PULLUP);
+  Serial.begin(9600);
+  
+  ini_msg();
+  delay(2000);
+}
+
+void loop() {
+  lcd.clear();
+  seviche = digitalRead(seviche_pin);
+  Serial.println(seviche);
+  if(seviche == HIGH){
+    timeMode();
+  }else{
+    ini_msg();
+  }
+
+  delay(100);
+//  digitalWrite(relay, HIGH);
+}
